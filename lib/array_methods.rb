99f0260d9b551ee8eccd16f042b3a4022bfa3260
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-10 20:24:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,25 +5,25 @@ end
 
 def using_sort(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    array.sort(element)
+    array.sort
 end
 
 def using_reverse(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    array.reverse(element)
+    array.reverse
 end
 
 def using_first(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    array.first(element)
+    array.first
 end
 
 def using_last(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    array.last(element)
+    array.last
 end
 
 def using_size(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    array.size(element)
+    array.size
 end
